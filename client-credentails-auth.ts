c71cd4bc5d1aea7cc72abd49ef4bc96d7a91439f
saif-at-scalekit <saif.shaik@scalekit.com>
2025-03-25 12:19:52 +0530

    Update client-credentials-auth.ts to modify the scope from 'offline_access' to 'offline_access client_credentials'.

--- client-credentails-auth.ts
+++ client-credentails-auth.ts
@@ -11,7 +11,7 @@ const config = {
   clientId: process.env.SCALEKIT_CLIENT_ID,
   clientSecret: process.env.SCALEKIT_CLIENT_SECRET,
   tokenUrl: `${process.env.SCALEKIT_ENVIRONMENT_URL}/oauth/token`,
-  scope: 'offline_access', // Optional: specify required scopes
+  scope: 'offline_access client_credentials',
 };
 
 main();
